d4da4e1f08cc245b5a32d6c83d95a939fb95cce4
Hareem Bilal <91612899+hareem7bilal@users.noreply.github.com>
2024-06-21 19:34:04 +0500

    first commit

---
+++ Start/Files/Directories/Program.cs
@@ -0,0 +1,46 @@
+﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+// Creating and Deleting Directories
+
+const string dirname = "TestDir";
+
+// TODO: Create a Directory if it doesn't already exist
+if(!Directory.Exists(dirname)){
+    Directory.CreateDirectory(dirname);
+}
+else{
+     Directory.Delete(dirname);
+}
+
+
+// TODO: Get the path for the current directory
+string curpath = Directory.GetCurrentDirectory();
+Console.WriteLine($"Current directory is {curpath}");
+
+
+// TODO: Just like with files, you can retrieve info about a directory
+DirectoryInfo di  = new DirectoryInfo(curpath);
+Console.WriteLine($"{di.Name}");
+Console.WriteLine(di.Parent);
+Console.WriteLine(di.CreationTime);
+
+
+// TODO: Enumerate the contents of directories
+Console.WriteLine("Just directories:");
+List<string> thedirs = new List<string>(Directory.EnumerateDirectories(curpath));
+foreach (string dir in thedirs){
+    Console.WriteLine(dir);
+}
+Console.WriteLine("---------------");
+
+Console.WriteLine("Just files:");
+List<string> thefiles = new List<string>(Directory.EnumerateFiles(curpath));
+foreach (string file in thefiles){
+    Console.WriteLine(file);
+}
+Console.WriteLine("---------------");
+
+Console.WriteLine("All directory contents:");
+List<string> thedirsnfiles = new List<string>(Directory.EnumerateFileSystemEntries(curpath));
+foreach (string item in thedirsnfiles){
+    Console.WriteLine(item);
+}
---
+++ Start/Strings/Searching/Program.cs
@@ -0,0 +1,28 @@
+﻿// LinkedIn Learning Course .NET Programming with C# by Joe Marini
+// Example file for searching string content
+
+string teststr = "The quick brown Fox jumps over the lazy Dog";
+
+// TODO: Contains determines whether a string contains certain content
+Console.WriteLine(teststr.Contains("fox"));
+Console.WriteLine(teststr.Contains("Fox"));
+Console.WriteLine(teststr.Contains("fox", StringComparison.CurrentCultureIgnoreCase));
+
+// TODO: StartsWith and EndsWith determine if a string starts 
+// or ends with a given test string
+Console.WriteLine(teststr.StartsWith("the", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine(teststr.EndsWith("dog"));
+
+// TODO: IndexOf, LastIndexOf finds the index of a substring
+Console.WriteLine(teststr.IndexOf("the"));
+Console.WriteLine(teststr.LastIndexOf("the"));
+Console.WriteLine(teststr.IndexOf("the", StringComparison.CurrentCultureIgnoreCase));
+
+
+// TODO: Determining empty, null, or whitespace
+string str1 = null;
+string str2 = "   ";
+string str3 = String.Empty;
+Console.WriteLine(String.IsNullOrEmpty(str1));
+Console.WriteLine(String.IsNullOrEmpty(str3));
+Console.WriteLine(String.IsNullOrWhiteSpace(str2));
